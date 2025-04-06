a06992fcf4eeb064187b94f21ff6bebcabdb7d55
stevenmjl <stevenmjl.24@gmail.com>
2025-04-06 00:07:37 -0400

    Creando el Contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CyH_Techno_Store.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+}
