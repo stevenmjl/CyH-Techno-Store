a2d06850f1d997357ae07d67a11f84f95bd8d416
stevenmjl <stevenmjl.24@gmail.com>
2025-04-06 00:01:50 -0400

    Agregando el paquete de Bootstrap

--- Program.cs
+++ Program.cs
@@ -6,6 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+// Inyeción de servicios a usar
+builder.Services.AddBlazorBootstrap();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
