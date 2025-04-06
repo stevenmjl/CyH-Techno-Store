9c0226f0af9aeacf33e2012a9b88b860c833c551
stevenmjl <stevenmjl.24@gmail.com>
2025-04-06 01:03:59 -0400

    Agregando Usuario al Contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using CyH_Techno_Store.Models;
 
 namespace CyH_Techno_Store.DAL;
 
 public class Contexto : DbContext
 {
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    public DbSet<UserAccount> UserAccounts { get; set; }
 }
