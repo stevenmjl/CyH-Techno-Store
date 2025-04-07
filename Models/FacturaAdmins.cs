f6e0eb7337c6fc720e75316fc53dfb29ee6ad9a8
stevenmjl <stevenmjl.24@gmail.com>
2025-04-07 03:21:32 -0400

    Creando modelo Factura para Admins

---
+++ Models/FacturaAdmins.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CyH_Techno_Store.Models;
+public class FacturaAdmins
+{
+    [Key]
+    public int FacturaAdminId { get; set; }
+
+    [DataType(DataType.DateTime)]
+    public DateTime FechaRegistro { get; set; }
+
+    [Required(ErrorMessage = "El usuario es obligatorio")]
+    public int UsuarioId { get; set; }
+    public Usuarios? Usuarios { get; set; }
+
+    [Required(ErrorMessage = "La categoría es obligatoria")]
+    public int ProveedorId { get; set; }
+    public Proveedores? Proveedores { get; set; }
+
+    public ICollection<DetalleFacturaAdmins> DetalleFacturaAdmin { get; set; } = new List<DetalleFacturaAdmins>();
+}
