526431684be22d7f85701ad0964aa7b6a89f6343
stevenmjl <stevenmjl.24@gmail.com>
2025-04-07 03:25:02 -0400

    Ajustando datos de modelo Productos

--- Models/Productos.cs
+++ Models/Productos.cs
@@ -2,38 +2,38 @@
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CyH_Techno_Store.Models;
-
-public class Productoss
+public class Productos
 {
     [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int ProductosId { get; set; }
+    public int ProductoId { get; set; }
 
     [Required(ErrorMessage = "El nombre del Productos es obligatorio")]
     [StringLength(100, ErrorMessage = "El nombre no puede exceder 100 caracteres")]
-    public string Nombre { get; set; }
+    public string? Nombre { get; set; }
 
-    [Required(ErrorMessage = "El precio es obligatorio")]
+    [Required(ErrorMessage = "El precio unitario es obligatorio")]
     [Column(TypeName = "decimal(18,2)")]
     [Range(0.01, double.MaxValue, ErrorMessage = "El precio debe ser mayor a 0")]
-    public decimal Precio { get; set; }
+    public decimal PrecioUnitario { get; set; }
 
     [StringLength(256, ErrorMessage = "La descripción no puede exceder 256 caracteres")]
-    public string Descripcion { get; set; }
+    public string? Descripcion { get; set; }
 
     [Required(ErrorMessage = "El stock es obligatorio")]
     [Range(0, int.MaxValue, ErrorMessage = "El stock no puede ser negativo")]
     public int Stock { get; set; }
 
     [DataType(DataType.DateTime)]
-    public DateTime FechaRegistro { get; set; } = DateTime.Now;
-   
+    public DateTime FechaRegistro { get; set; }
+
     [StringLength(255, ErrorMessage = "La ruta de la imagen no puede exceder 255 caracteres")]
-    public string ImagenUrl { get; set; } 
+    public string? ImagenUrl { get; set; } 
     
-    public virtual ICollection<DetalleFacturass> DetallesFacturas { get; set; } = new List<DetalleFacturass>();
+    public virtual ICollection<DetalleFacturas> DetalleFactura { get; set; } = new List<DetalleFacturas>();
+
+    public virtual ICollection<DetalleFacturaAdmins> DetalleFacturaAdmin { get; set; } = new List<DetalleFacturaAdmins>();
 
     [Required(ErrorMessage = "La categoría es obligatoria")]
     public int CategoriaId { get; set; }
-    public Categoria Categoria { get; set; }
+    public Categoria? Categoria { get; set; }
 }
